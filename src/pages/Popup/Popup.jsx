d1724c758a40357b8be91d383916aee02bfbe00c
huckjoo <huckjoo96@naver.com>
2022-02-24 14:30:46 +0900

    refactor: refactor extension complete

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,7 +3,6 @@ import './Popup.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const cheerio = require('cheerio');
 const axios = require('axios');
 let new_product;
 
@@ -12,15 +11,6 @@ const Popup = React.memo(function Popup(props) {
   // const [products, setProducts] = useState([]);
   // const [curProducts, setCurProducts] = useState({});
   // const [isLoading, setIsLoading] = useState(true);
-
-  // chrome.storage.local.get(['moba'], function (items) {
-  //   if (typeof items.moba == 'undefined') {
-  //     console.log('ㅜㅡㅜ');
-  //   } else {
-  //     console.log(items.moba, 'items.count');
-  //     console.log(items, 'items');
-  //   }
-  // });
 
   let authToken = '';
   chrome.storage.local.get(['userStatus'], function (items) {
@@ -49,8 +39,6 @@ const Popup = React.memo(function Popup(props) {
           `;
         }
         myBasket.innerHTML += temp;
-        // myBasket.addClass("")
-        // setProducts(Response.data);
       })
       .catch((Error) => {
         console.log(Error);
@@ -61,175 +49,51 @@ const Popup = React.memo(function Popup(props) {
     { currentWindow: true, active: true },
     async function (tabs) {
       const shopUrl = tabs[0].url;
-      chrome.storage.local.set({ moba: shopUrl });
+      chrome.storage.local.set({ moba: shopUrl }); // url chrome storage 저장
 
-      chrome.tabs.sendMessage(tabs[0].id, { shopUrl: shopUrl }, (response) => {
-        console.log(response, 'response!!!!!!!@'); // Yeah
-      });
-
-      new_product = await parse_product(shopUrl);
       chrome.storage.local.get(['products'], function (items) {
         console.log(items.products, 'item.products');
+        new_product = items.products;
+        let imageBox = document.querySelector('#imageBox');
+        let totalImg = '';
+
+        let imageUrl = new_product.img;
+        let product_name = new_product.product_name;
+        let sale_price = new_product.sale_price;
+        let shop_name = new_product.shop_name;
+
+        totalImg = `
+          <div class='image__container'>
+          <img class='currentImg' src=${imageUrl} alt='img1'/>
+          </div>
+          <div class='image__description'>
+          <p>${shop_name}</p>
+          <p>${product_name}</p>
+          <p>${sale_price} 원</p>
+          </div>
+          `;
+        imageBox.innerHTML = totalImg;
       });
-      let imageBox = document.querySelector('#imageBox');
-      let totalImg = '';
-
-      let imageUrl = new_product.img;
-      let product_name = new_product.product_name;
-      let sale_price = new_product.sale_price;
-      let shop_name = new_product.shop_name;
-
-      totalImg = `
-        <div class='image__container'>
-        <img class='currentImg' src=${imageUrl} alt='img1'/>
-        </div>
-        <div class='image__description'>
-        <p>${shop_name}</p>
-        <p>${product_name}</p>
-        <p>${sale_price} 원</p>
-        </div>
-        `;
-      imageBox.innerHTML = totalImg;
+      chrome.storage.local.get(['products'], function (items) {
+        console.log(items, 'remove 전 products');
+      });
+      chrome.storage.local.get(['moba'], function (items) {
+        console.log(items, 'remove 전 moba');
+      });
+      chrome.storage.local.remove(['products', 'moba'], function () {
+        var error = chrome.runtime.lastError;
+        if (error) {
+          console.error(error);
+        }
+      });
+      chrome.storage.local.get(['products'], function (items) {
+        console.log(items, 'remove 후 products');
+      });
+      chrome.storage.local.get(['moba'], function (items) {
+        console.log(items, 'remove 후 moba');
+      });
     }
   );
-  // w-concept
-  function w_concept(html, url) {
-    let shop_name, shop_url, img_url, product_name, price, sale_price;
-    const $ = cheerio.load(html); // html load
-
-    product_name = $("meta[property='og:description']").attr('content');
-    price = $("meta[property='eg:originalPrice']").attr('content');
-    sale_price = $("meta[property='eg:salePrice']").attr('content');
-    shop_name = $("meta[property='og:site_name']").attr('content');
-    shop_url = url;
-    img_url = $("meta[property='og:image']").attr('content');
-
-    const new_product = {
-      product_name: product_name,
-      price: price,
-      sale_price: sale_price,
-      shop_name: shop_name,
-      shop_url: shop_url,
-      img: img_url,
-    };
-
-    return new_product;
-  }
-
-  // 무신사
-  function musinsa(html, url) {
-    let shop_name, shop_url, img_url, product_name, price, sale_price;
-    const $ = cheerio.load(html); // html load
-
-    product_name = $(
-      '#page_product_detail > div.right_area.page_detail_product > div.right_contents.section_product_summary > span > em'
-    ).text();
-    price = $('#goods_price').text().trim();
-    console.log(price);
-    // price parsing - e.g. 110,000원 -> 110000
-    price = Number(
-      price
-        .slice(0, -1)
-        .split(',')
-        .reduce((a, b) => a + b)
-    );
-
-    sale_price = $(
-      '#sPrice > ul > li > span.txt_price_member.m_list_price'
-    ).text();
-    console.log(sale_price);
-    sale_price = Number(
-      sale_price
-        .slice(0, -1)
-        .split(',')
-        .reduce((a, b) => a + b)
-    );
-    shop_name = 'Musinsa';
-    shop_url = url;
-    img_url = $("meta[property='og:image']").attr('content');
-
-    const new_product = {
-      product_name: product_name,
-      price: Number(price),
-      sale_price: sale_price,
-      shop_name: shop_name,
-      shop_url: shop_url,
-      img: img_url,
-    };
-    return new_product;
-  }
-
-  // 브랜디
-  function brandi(html, url) {
-    let shop_name, img_url, product_name, price, sale_price, shop_url;
-    const $ = cheerio.load(html); // html load
-
-    shop_name = '브랜디';
-    shop_url = url;
-    img_url = $("meta[property='og:image']").attr('content');
-    product_name = $(
-      '#container > div > div.wrap-products-info > div.wrap-detail_info > div.detail_basic-info > div.detail_title_area > h1'
-    ).text();
-    price = $(
-      '#container > div > div.wrap-products-info > div.wrap-detail_info > div.detail_basic-info > div.detail-price-wrapper.hideFinalPriceSection > div > div > span > span'
-    ).text();
-    price = Number(price.split(',').reduce((a, b) => a + b));
-    sale_price = $(
-      '#container > div > div.wrap-products-info > div.wrap-detail_info > div.detail_basic-info > div.detail-price-wrapper.hideFinalPriceSection > div > div > em > span'
-    ).text();
-    sale_price = Number(sale_price.split(',').reduce((a, b) => a + b));
-
-    const new_product = {
-      product_name: product_name,
-      price: price,
-      sale_price: sale_price,
-      shop_name: shop_name,
-      shop_url: shop_url,
-      img: img_url,
-    };
-    console.log(new_product);
-
-    return new_product;
-  }
-
-  async function parse_product(url) {
-    let new_product;
-    console.log(url);
-    const split_url = url.split('/');
-    const cur_shop = split_url[2];
-    // 서비스 가능한 사이트만 req 요청 보내기
-    if (
-      ['www.wconcept.co.kr', 'store.musinsa.com', 'www.brandi.co.kr'].includes(
-        cur_shop
-      )
-    ) {
-      //contents에서 해보자
-
-      await axios
-        .get(url)
-        .then((dataa) => {
-          const html = dataa.data;
-          switch (cur_shop) {
-            case 'www.wconcept.co.kr':
-              new_product = w_concept(html, url);
-              break;
-            case 'store.musinsa.com':
-              new_product = musinsa(html, url);
-              break;
-            case 'www.brandi.co.kr':
-              new_product = brandi(html, url);
-              break;
-            default:
-              break;
-          }
-        })
-        .catch(
-          // 리퀘스트 실패 - then 보다 catch 가 먼저 실행됨..
-          console.log('get shopping mall html request is failed')
-        );
-    }
-    return new_product;
-  }
 
   async function removeBackground(new_product) {
     const canvas = document.querySelector('#myCanvas');
@@ -265,7 +129,7 @@ const Popup = React.memo(function Popup(props) {
       console.log(removedBgImg, 'return 전 removedBg');
 
       //누끼딴거 넣어주기
-      new_product.removedBgImg = removedBgImg;
+      // new_product.removedBgImg = removedBgImg;
       console.log(new_product, 'new_product@#!#!');
 
       chrome.storage.local.get(['userStatus'], function (items) {
